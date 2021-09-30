7e4cac0bfe7ad09847a9d9d313b5acb5bd8eba81
deborahalynne <igormat4us@gmail.com>
2021-09-30 20:44:44 -0300

    Sync

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule} from '@angular/material/icon';
 import { MatFormFieldModule} from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { TaskService } from './services/tasks.service';
+import { TasksService } from './services/tasks.service';
 
 @NgModule({
   declarations: [
@@ -27,7 +27,7 @@ import { TaskService } from './services/tasks.service';
     MatInputModule,
     MatFormFieldModule,
     MatInputModule,
-    TaskService
+    TasksService
   ],
   providers: [],
   bootstrap: [AppComponent]
