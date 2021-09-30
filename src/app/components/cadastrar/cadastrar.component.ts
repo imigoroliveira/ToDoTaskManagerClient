e21293ff70dc84a9ac68914d3b0ef9e161d2fe74
GabrielMarchiorato <gabrielmarchiorato1@outlook.com>
2021-09-30 20:34:53 -0300

    Correção do service

--- src/app/components/cadastrar/cadastrar.component.ts
+++ src/app/components/cadastrar/cadastrar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Task } from 'src/app/models/task';
+import { TaskService } from 'src/app/services/task.service';
+
 
 @Component({
   selector: 'app-cadastrar',
@@ -7,9 +11,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarComponent implements OnInit {
 
-  constructor() { }
+  nome!: string;
+  descricao!: string;
+  prioridade!: string;
+  data!: string;
 
-  ngOnInit() {
-  }
 
+  constructor(private service: TaskService, private router: Router) { }
+
+  ngOnInit(): void{}
+
+  cadastrar(): void
+    {
+        let task : Task = {
+            nome: this.nome,
+            descricao: this.descricao,
+            prioridade: this.prioridade,
+            data: this.data,
+        };
+        //Chamar o create do service
+        this.service.create(task).subscribe(task => {
+            console.log(task);
+            this.router.navigate(["task/listar"]);
+        });
+    }
 }
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,32 +1,25 @@
-import { Task } from '../models/task';
-import { FiltroPesquisa } from './../models/FiltroPesquisa';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { Task } from '../models/task';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class TaskService {
-  private baseUrl = `${environment.apiUrl}/task`;
 
-  constructor(private http: HttpClient) {}
-
-  create(task: Task): Observable<Task> {
-    return this.http.post<Task>(
-      `${this.baseUrl}/create`,
-      task
-    );
-  }
-
-  list(): Observable<Task[]> {
-    return this.http.get<Task[]>(`${this.baseUrl}/list`);
-  }
+    private baseURL = "http://localhost:5000/api/task";
 
 
+    constructor(private http: HttpClient) { }
 
-  }
+    list(): Observable<Task[]>
+    {
+        return this.http.get<Task[]>(`${this.baseURL}/list`);
+    }
 
+    create(task: Task): Observable<Task>
+    {
+        return this.http.post<Task>(`${this.baseURL}/create`, task);
+    }
 }
